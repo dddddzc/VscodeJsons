39315456090fd57f247a8371ff68ddf37f6bc827
inlabdzc <202321010415@std.uestc.edu.cn>
2024-10-10 20:34:58 +0800

    wsl jsons

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    int j = 2;
+    cout << i << endl;
+    for(int k = 3; k < 10; k++) {
+        cout << j << endl;
+    }
+    return 0;
+}
